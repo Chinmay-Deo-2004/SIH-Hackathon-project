01b7d3f96162ab3c54db596b11a1a044eb1b9278
Chinmay Deo <chinmaydeo2004@gmail.com>
2023-09-27 01:35:50 +0530

    fixed bug

--- SIH_Project/SIH_Project/settings.py
+++ SIH_Project/SIH_Project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pdcbze^2a21k7j%iz67ti2=4^o)#6xre@dc2+zi8h3fdhx*mq&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -37,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.Apiconfig',
-    'rest_framework'
+    'rest_framework',
+    'Api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
